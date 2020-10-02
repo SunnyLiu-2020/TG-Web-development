754ac6e6400f733530eb1ce3a3ef64a04a996629
Sunny Liu <SunnyLiu@protonmail.com>
2020-10-02 23:46:48 +0200

    Daily work Fri, Oct 2, 2020
    Daily work Fri, Oct 2, 2020

---
+++ javascript/3-functions/opdracht-8.js
@@ -0,0 +1,124 @@
+function passNoPass(grade) {
+    // let grades = ["ww", "jnw", "kw", "sw", "jcw", "gw", "wna", "jnna", "kna", "sna", "jcna", "gna", "wnl", "jnnl", "knl", "snl", "jcnl", "gnl", "we", "jne", "ke", "se", "jce", "ge", "wt", "wb", "jnt", "jnb", "kt", "kb", "st", "sb", "jct", "jcb", "gt", "gb"]
+
+    // let grade;
+
+    // for (let i = 0; i < grades.length; i++) {
+    //     grade = grades[i];
+    // }
+    
+    // document.getElementById(grades[i]).value;
+
+    let grade = document.getElementsByClassName("grade").value;
+
+    if (grade < 5.5) {
+        document.getElementsByClassName("grade").style.background = "#ff6666";
+    } else if (grade >= 5.5 && grade < 7.5) {
+        document.getElementsByClassName("grade").style.background = "#66ff66";
+    } else if (grade >= 7.5 && grade <= 10) {
+        document.getElementsByClassName("grade").style.background = "#00ff00"
+    }
+}
+
+passNoPass(grade);
+
+// function meanLanguages() {}
+
+// function meanSciences() {}
+
+// function nextGrade() {}
+
+// document.getElementById("").style.background = "#ff6666"; // red
+// document.getElementById("").style.background = "#66ff66"; // ligrn
+// document.getElementById("").style.background = "#00ff00"; // grn
+
+// document.getElementById("ww").style.background = "#ff6666";
+// document.getElementById("jnw").style.background = "#66ff66";
+// document.getElementById("kw").style.background = "#00ff00";
+
+// document.getElementById("ww").style.background = "#ff6666";
+// document.getElementById("jnw").style.background = "#66ff66";
+// document.getElementById("kw").style.background = "#00ff00";
+// document.getElementById("sw").style.background = "#ff6666";
+// document.getElementById("jcw").style.background = "#66ff66";
+// document.getElementById("gw").style.background = "#00ff00";
+// document.getElementById("wna").style.background = "#ff6666";
+// document.getElementById("jnna").style.background = "#66ff66";
+// document.getElementById("kna").style.background = "#00ff00";
+// document.getElementById("sna").style.background = "#ff6666";
+// document.getElementById("jcna").style.background = "#66ff66";
+// document.getElementById("gna").style.background = "#00ff00";
+// document.getElementById("wnl").style.background = "#ff6666";
+// document.getElementById("jnnl").style.background = "#66ff66";
+// document.getElementById("knl").style.background = "#00ff00";
+// document.getElementById("snl").style.background = "#ff6666";
+// document.getElementById("jcnl").style.background = "#66ff66";
+// document.getElementById("gnl").style.background = "#00ff00";
+// document.getElementById("we").style.background = "#ff6666";
+// document.getElementById("jne").style.background = "#66ff66";
+// document.getElementById("ke").style.background = "#00ff00";
+// document.getElementById("se").style.background = "#ff6666";
+// document.getElementById("jce").style.background = "#66ff66";
+// document.getElementById("ge").style.background = "#00ff00";
+// document.getElementById("wt").style.background = "#ff6666";
+// document.getElementById("wb").style.background = "#66ff66";
+// document.getElementById("wo").style.background = "#00ff00";
+// document.getElementById("jnt").style.background = "#ff6666";
+// document.getElementById("jnb").style.background = "#66ff66";
+// document.getElementById("jno").style.background = "#00ff00";
+// document.getElementById("kt").style.background = "#ff6666";
+// document.getElementById("kb").style.background = "#66ff66";
+// document.getElementById("ko").style.background = "#00ff00";
+// document.getElementById("st").style.background = "#ff6666";
+// document.getElementById("sb").style.background = "#66ff66";
+// document.getElementById("so").style.background = "#00ff00";
+// document.getElementById("jct").style.background = "#ff6666";
+// document.getElementById("jcb").style.background = "#66ff66";
+// document.getElementById("jco").style.background = "#00ff00";
+// document.getElementById("gt").style.background = "#ff6666";
+// document.getElementById("gb").style.background = "#66ff66";
+// document.getElementById("go").style.background = "#00ff00";
+
+// document.getElementById("ww").value = ;
+// document.getElementById("jnw").value = ;
+// document.getElementById("kw").value = ;
+// document.getElementById("sw").value = ;
+// document.getElementById("jcw").value = ;
+// document.getElementById("gw").value = ;
+// document.getElementById("wna").value = ;
+// document.getElementById("jnna").value = ;
+// document.getElementById("kna").value = ;
+// document.getElementById("sna").value = ;
+// document.getElementById("jcna").value = ;
+// document.getElementById("gna").value = ;
+// document.getElementById("wnl").value = ;
+// document.getElementById("jnnl").value = ;
+// document.getElementById("knl").value = ;
+// document.getElementById("snl").value = ;
+// document.getElementById("jcnl").value = ;
+// document.getElementById("gnl").value = ;
+// document.getElementById("we").value = ;
+// document.getElementById("jne").value = ;
+// document.getElementById("ke").value = ;
+// document.getElementById("se").value = ;
+// document.getElementById("jce").value = ;
+// document.getElementById("ge").value = ;
+// document.getElementById("wt").value = ;
+// document.getElementById("wb").value = ;
+// document.getElementById("jnt").value = ;
+// document.getElementById("jnb").value = ;
+// document.getElementById("kt").value = ;
+// document.getElementById("kb").value = ;
+// document.getElementById("st").value = ;
+// document.getElementById("sb").value = ;
+// document.getElementById("jct").value = ;
+// document.getElementById("jcb").value = ;
+// document.getElementById("gt").value = ;
+// document.getElementById("gb").value = ;
+
+// document.getElementById("wo").value = ;
+// document.getElementById("jno").value = ;
+// document.getElementById("ko").value = ;
+// document.getElementById("so").value = ;
+// document.getElementById("jco").value = ;
+// document.getElementById("go").value = ;
